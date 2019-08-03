d3f30e730b3cffa28080c19b6474519fe736b4f9
zzq <jjck_zzq@qq.com>
2019-08-03 18:01:42 +0800

    文件上传功能

---
+++ JJCKManager/JJCKManager/Controllers/FileController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JJCKManager.Controllers
+{
+    public class FileController : Controller
+    {
+        // GET: File
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult UploadFile()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UploadFile(HttpPostedFileBase file)
+        {
+            var fileName = file.FileName;
+            var filePath = Server.MapPath(string.Format("~/{0}", "UpFile")); //确定上传文件保存的路径
+            file.SaveAs(Path.Combine(filePath,fileName));
+            return View();
+        }
+    }
+}
