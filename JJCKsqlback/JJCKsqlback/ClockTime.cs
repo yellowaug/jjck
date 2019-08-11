64b7967c24955966230a0603e43650a0ca084f7a
zzq <2943917172@qq.com>
2019-08-11 20:27:36 +0800

    添加了根据天数运行的定时程序

--- JJCKsqlback/JJCKsqlback/ClockTime.cs
+++ JJCKsqlback/JJCKsqlback/ClockTime.cs
@@ -7,22 +7,43 @@ using System.Threading.Tasks;
 
 namespace JJCKsqlback
 {
+    delegate void SQLshell();
     class ClockTime
     {
         ///<summary>
         ///定时器
-        ///一个简单的定时运行的程序算是实现了
+        ///根据设定的时间决定程序每天是几点运行的。
         ///</summary>
-        public void TimeCompare()
+        public void TimeCompare(SQLshell shell)//传入委托，自动备份方法在判断时间
         {
             DateTime localtime = DateTime.Now;
             int hournow = localtime.Hour;
             int minuteNow = localtime.Minute;
             int seconNow = localtime.Second;
-            if (hournow==23&&minuteNow==30&&seconNow==00)
+            if (hournow==11&&minuteNow==57&&seconNow==00)
             {
-                Console.WriteLine("就是这个时候了");
+                               
                 Console.WriteLine("数据库正在备份中。。。。。");
+                shell();
+            }
+            else
+            {
+                Console.WriteLine(localtime.ToString("yyyy-MM-dd hh:mm:ss"));
+            }
+        }
+        /// <summary>
+        /// 根据设定的时间，判断程序要间隔几天执行
+        /// </summary>
+        /// <param name="shell"></param>
+        public void DayCompare(SQLshell shell)
+        {
+            DateTime localtime = DateTime.Now;
+            int dayNow = localtime.Day;
+            var comPare = dayNow % 4;
+            Console.WriteLine(comPare);
+            if (comPare == 0)
+            {
+                shell();
             }
             else
             {
--- JJCKsqlback/JJCKsqlback/Program.cs
+++ JJCKsqlback/JJCKsqlback/Program.cs
@@ -8,12 +8,18 @@ namespace JJCKsqlback
 {
     class Program
     {
+        /// <summary>
+        /// 通过调用委托，将自动备份方法传入定时方法中
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            SqlAction sqlAction = new SqlAction();
+            SQLshell autobackshell = sqlAction.DataBaseContronal;           
             while (true)
             {
                 ClockTime clockTime = new ClockTime();
-                clockTime.TimeCompare();
+                clockTime.TimeCompare(autobackshell);
                 Thread.Sleep(100);
                 Console.Clear();
             }
