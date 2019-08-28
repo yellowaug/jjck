75b4d7fe86f5bf12ba067f03ec5cd19dd687937b
专业各种大 <oo@DESKTOP-S9O1S2N>
2019-08-28 16:57:00 +0800

    创建数据访问层

---
+++ CSspider/DataAccess.Entity/BaseEntity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DataAccess.Entity
+{
+    public  abstract class BaseEntity<T>
+    {
+        /// <summary>
+        /// 唯一编号
+        /// </summary>
+        /// 
+        [Key]
+        public T ID { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateDateTime { get; set; }
+
+
+        /// <summary>
+        /// 更新时间
+        /// </summary>
+        public DateTime UpdateDateTime { get; set; }
+
+
+        /// <summary>
+        /// 是否删除
+        /// </summary>
+        public bool IsDelete { get; set; }
+}
+}
---
+++ CSspider/DataAccess.EntityFramework/BaseDbContext.cs
@@ -0,0 +1,63 @@
+﻿using DataAccess.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.EntityFramework
+{
+    public class BaseDbContext : DbContext
+    {
+        public BaseDbContext(DbContextOptions options)
+      : base(options) {}
+
+        /// <summary>
+        /// 新闻表
+        /// </summary>
+        public DbSet<New> News { get; set; }
+
+        /// <summary>
+        /// 股票表
+        /// </summary>
+        public DbSet<Stock> Stocks { get; set; }
+
+
+        /// <summary>
+        /// 分红表
+        /// </summary>
+        public DbSet<Dividend>  Dividends { get; set; }
+
+        public override int SaveChanges()
+        {
+
+            foreach (var entry in ChangeTracker.Entries())
+            {
+                switch (entry.State)
+                {
+                    //新增
+                    case EntityState.Added:
+                        entry.CurrentValues["ID"] = Guid.NewGuid();
+                        entry.CurrentValues["CreateDateTime"] = DateTime.Now;    //创建时间
+                        break;
+
+                    //删除
+                    case EntityState.Deleted:
+                        entry.State = EntityState.Unchanged;
+                        entry.CurrentValues["IsDelete"] = true;                  //软删除
+                        entry.CurrentValues["UpdateDateTime"] = DateTime.Now;    //更新时间
+                        break;
+
+
+                    //修改
+                    case EntityState.Modified:
+                        entry.Property("CreateDateTime").IsModified = false;    //创建时间不允许修改
+                        entry.CurrentValues["UpdateDateTime"] = DateTime.Now;   //更新时间
+                        break;
+
+                }
+            }
+
+            return base.SaveChanges();
+        }
+    }
+}
