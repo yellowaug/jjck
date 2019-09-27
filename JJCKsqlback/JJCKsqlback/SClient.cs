d9aa58ff0e8e5f4a2c9d3ec0d76dd826a6e8fb2f
zzq <jjck_zzq@qq.com>
2019-09-27 17:19:26 +0800

    添加了Socket客户端发送数据库文件的功能，但是还没有引用，明天再进一步修改

---
+++ JJCKsqlback/JJCKsqlback/SClient.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+using System.Threading;
+
+namespace SocketClient
+{
+    class SClient
+    {
+        public void Client(FilePack filepackInfos)
+        {
+            //配置Sock连接信息
+            int port = 2000;
+            string host = "127.0.0.1";
+            IPAddress ip = IPAddress.Parse(host);
+            IPEndPoint iPEnd = new IPEndPoint(ip, port);
+            Socket sendFileCount = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Console.WriteLine("发送文件个数中......");
+            sendFileCount.Connect(iPEnd);
+            byte[] packFilecount = new ASCIIEncoding().GetBytes(filepackInfos.FileName.Count.ToString());
+            sendFileCount.Send(packFilecount, packFilecount.Length, 0);
+            sendFileCount.Close();
+            Console.WriteLine("文件个数发送完成");
+            //for (int i = 0; i < filepackInfos.FileName.Count; i++)
+            //{
+
+            //    发送文件名
+            //    byte[] packFileName = new ASCIIEncoding().GetBytes(filepackInfos.FileName[i]);
+            //    c.Send(packFileName, packFileName.Length, 0);
+            //    Thread.Sleep(500);
+            //发送文件的大小信息
+            //byte[] packFileSize = new ASCIIEncoding().GetBytes(filepackInfos.FileSize[i].ToString());
+            //c.Send(packFileSize, packFileSize.Length, 0);
+            //Thread.Sleep(500);
+            //发送文件数据包
+            //for (int j = 0; j < filepackInfos.FilePackList.Count; j++)
+            //{
+            //    c.Send(filepackInfos.FilePackList[j], filepackInfos.FilePackList[j].Length, 0);
+            //    float sendPer = (float)j / (float)filepackInfos.FilePackList.Count;
+            //    Console.WriteLine($"文件发送{sendPer * 100}%");
+            //}
+            //}
+            Socket sendFileName = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Console.WriteLine("发送文件名连接中......");
+            sendFileName.Connect(iPEnd);
+            foreach (var itemfileName in filepackInfos.FileName)
+            {
+                byte[] packFileName = new ASCIIEncoding().GetBytes(itemfileName);
+                sendFileName.Send(packFileName, packFileName.Length, 0);
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("发送文件名完成");
+            sendFileName.Close();
+            //发送文件的大小信息
+            Socket sendFileSize = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Console.WriteLine("连接中......");
+            sendFileSize.Connect(iPEnd);
+            foreach (var itemfileSize in filepackInfos.FileSize)
+            {
+                byte[] packFileSize = new ASCIIEncoding().GetBytes(itemfileSize.ToString());
+                sendFileSize.Send(packFileSize, packFileSize.Length, 0);
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("发送文件大小信息完成");
+            sendFileSize.Close();
+            //byte[] packCount = new ASCIIEncoding().GetBytes(fileInfos.Count.ToString());
+            //c.Send(packCount, packCount.Length, 0);
+
+            //发送文件数据包,整个数据包发出去，不做切割，然后在服务端进行切割复原。
+            //for (int i = 0; i < filepackInfos.FilePackList.Count; i++)
+            //{
+
+            //    c.Send(filepackInfos.FilePackList[i], filepackInfos.FilePackList[i], 0);
+            //    float sendPer = (float)i / (float)filepackInfos.FilePackList.Count;
+            //    Console.WriteLine($"文件发送{sendPer * 100}%");
+            //}
+            Socket sendFilePack = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sendFilePack.Connect(iPEnd);
+            foreach (var itemfilePack in filepackInfos.FilePackList)
+            {
+                Thread.Sleep(1);
+                for (int i = 0; i < itemfilePack.TempFilePackList.Count; i++)
+                {
+                    sendFilePack.Send(itemfilePack.TempFilePackList[i], itemfilePack.TempFilePackList[i].Length, 0);
+                    float sendPer = (float)i / (float)itemfilePack.TempFilePackList.Count;
+                    Console.WriteLine($"文件发送{sendPer * 100}%");
+                    Thread.Sleep(1);
+                }
+
+            }
+            sendFilePack.Close();
+            Console.WriteLine("发送数据包完成");
+            //foreach (var itembyte in fileInfos)
+            //{                
+            //    c.Send(itembyte, itembyte.Length, SocketFlags.None);
+            //}
+
+            //string recvStr = "";
+            //byte[] recvBytes = new byte[1024];
+            //int bytes;
+            //bytes = c.Receive(recvBytes, recvBytes.Length, 0);
+            //recvStr += Encoding.ASCII.GetString(recvBytes, 0, bytes);
+            //Console.WriteLine($"client get message{recvStr}");
+            //c.Close();
+
+        }
+    }
+}
+
---
+++ JJCKsqlback/JJCKsqlback/SocketFolderAction.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SocketClient
+{
+    /// <summary>
+    /// 存储文件名列表，文件大小列表，数据包列表
+    /// </summary>
+    public class FilePack
+    {
+        public List<string> FileName { get; set; }
+        public List<int> FileSize { get; set; }
+        public List<TempPack> FilePackList { get; set; }
+    }
+    /// <summary>
+    /// 存储未封装的数据包
+    /// </summary>
+    public class TempPack
+    {
+        public List<byte[]> TempFilePackList { get; set; }
+    }
+    /// <summary>
+    /// 读取文件夹中有多少文件的接口
+    /// </summary>
+    public interface IEumFiles
+    {
+        IEnumerable<FileInfo> EumFile(string folderPath);
+    }
+    /// <summary>
+    /// 读取文件内容封装成数据包的接口
+    /// </summary>
+    public interface IReadFile
+    {
+        FilePack ReadFile(IEnumerable<FileInfo> filePathlist);
+    }
+    public class FolderAction : IEumFiles, IReadFile
+    {
+
+        IEnumerable<FileInfo> IEumFiles.EumFile(string folderPath)
+        {
+            DirectoryInfo directory = new DirectoryInfo(folderPath);
+            var fileinfoEnu = directory.EnumerateFiles();
+            if (fileinfoEnu != null)
+            {
+                foreach (var file in fileinfoEnu)
+                {
+                    Console.WriteLine(file.FullName);
+                    Console.WriteLine(file.Length);
+                }
+            }
+            else
+            {
+                Console.WriteLine("文件夹为空");
+            }
+            return fileinfoEnu;
+        }
+        //将大文件切割成1024字节大小的小数据包，因为TCP/IP传输的数据包大小是有限制的
+        FilePack IReadFile.ReadFile(IEnumerable<FileInfo> filePathlist)
+        {
+            FilePack filePack = new FilePack();
+
+
+            List<string> fullName = new List<string>();
+            List<int> fileSize = new List<int>();
+            List<TempPack> tempPacks = new List<TempPack>();
+            foreach (var item in filePathlist)
+            {
+                TempPack tempPackList = new TempPack();
+                fullName.Add(item.Name);
+                fileSize.Add((int)item.Length);
+                tempPacks.Add(tempPackList);
+                List<byte[]> fileInfoPackList = new List<byte[]>();
+                int packLength = (int)item.Length % 1024;
+                if (packLength == 0)
+                {
+                    var fs = File.OpenRead(item.FullName);
+                    for (int i = 0; i < (int)item.Length / 1024; i++)
+                    {
+                        byte[] fscontext = new byte[1024];
+                        fs.Read(fscontext, 0, fscontext.Length);
+                        fileInfoPackList.Add(fscontext);
+                    }
+                    fs.Close();
+                    tempPackList.TempFilePackList = fileInfoPackList;
+
+                    //byte[] fscontext = new byte[item.Length];
+                    Console.WriteLine(item.Length);
+                    //var fs = File.OpenRead(item.FullName);
+                }
+                else if (packLength != 0)
+                {
+                    var fs = File.OpenRead(item.FullName);
+                    int ts = (int)item.Length / 1024;
+                    for (int i = 0; i < ts; i++)
+                    {
+                        byte[] fscontext = new byte[1024];
+                        fs.Read(fscontext, 0, fscontext.Length);
+                        fileInfoPackList.Add(fscontext);
+
+                    }
+
+                    byte[] lastPack = new byte[(int)item.Length % 1024];
+                    fs.Read(lastPack, 0, lastPack.Length);
+                    fileInfoPackList.Add(lastPack);
+                    fs.Close();
+                    tempPackList.TempFilePackList = fileInfoPackList;
+
+                }
+
+            }
+            filePack.FileName = fullName;
+            filePack.FileSize = fileSize;
+            filePack.FilePackList = tempPacks;
+            return filePack;
+
+
+
+
+        }
+    }
+}
